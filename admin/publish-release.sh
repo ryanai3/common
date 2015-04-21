5c2df277db65d4ccae3a6f05bc88ec4523040409
Michael Schmitz <michael@schmitztech.com>
2015-04-21 07:40:43 -0700

    Fix equality symbols.

--- admin/publish-release.sh
+++ admin/publish-release.sh
@@ -4,7 +4,7 @@ set -e
 
 # Publish to BinTray if the HEAD commit is tagged with a version number.
 
-if [ $PULL_REQUEST -eq "true" ]; then
+if [ "$PULL_REQUEST" = "true" ]; then
   echo "Shippable is building a pull request, not publishing."
   exit 0
 fi
@@ -12,7 +12,7 @@ fi
 headTag=$(git describe --exact-match ||:)
 echo "Current tag is: $headTag"
 
-if [[ PULL_REQUEST -ne "true" && headTag =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)? ]]; then
+if [[ PULL_REQUEST != "true" && headTag =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)? ]]; then
   echo "Going to release from tag $headTag."
   version=$(echo $headTag | sed -e s/^v//)
 
